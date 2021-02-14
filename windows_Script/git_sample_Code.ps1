651276b99e2a887b9b6456c3e8936abae9d68fb3
waka_prings <waka_taka@wakataka.mixh.jp>
2021-02-14 09:04:41 -0500

    ansible test 2021-02-14 2304

--- windows_Script/git_sample_Code.ps1
+++ windows_Script/git_sample_Code.ps1
@@ -22,7 +22,7 @@ git config --global alias.co checkout
 mkdir c:\typeScript_210213\
 cd c:\typeScript_210213\
 git init
-git commit -m "ansible test 2021-02-13 009"
+git commit -m "ansible test 2021-02-14 2302"
 git branch -M main
 git remote add origin https://github.com/wakaprings/TypeScriptTraining.git
 git push -u origin main
