cd30476d58a1df4f85684b57b8b10640de7b2035
root <root@localhost.localdomain>
2021-02-14 07:01:06 -0500

    20210214-Commitment

---
+++ windows_Script/FirewallDisable.ps1
@@ -0,0 +1,2 @@
+# FireWall無効化
+Get-NetFirewallProfile | Set-NetFirewallProfile -Enabled false
--- windows_Script/SyncLocalTime.ps1
+++ windows_Script/SyncLocalTime.ps1
@@ -1,5 +1,5 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 [Net.ServicePointManager]::SecurityProtocol
 Get-Date
-Invoke-WebRequest -Uri https://ntp-a1.nict.go.jp/cgi-bin/time
-Set-Date (Invoke-WebRequest -Uri https://ntp-a1.nict.go.jp/cgi-bin/time).Headers.Date
+#Invoke-WebRequest -Uri https://ntp-a1.nict.go.jp/cgi-bin/time
+#Set-Date (Invoke-WebRequest -Uri https://ntp-a1.nict.go.jp/cgi-bin/time).Headers.Date
