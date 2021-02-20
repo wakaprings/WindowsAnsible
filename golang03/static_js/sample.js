bc157ad2c45988457f41e703a89c6fe2f361b5a8
root <root@localhost.localdomain>
2021-02-20 03:03:38 -0500

    ansible Update 2021-02-20 17:03

--- golang03/static_js/sample.js
+++
@@ -1,9 +0,0 @@
-var list = [10, 30, 23, 45, 65, 30, 67];
-var tbl = d3.select("#result")  // div#result内に出力
-    .append("table")    // table要素を追加
-    .selectAll("tr")    // tr要素を対象にする
-    .data(list) // 出力するデータ
-    .enter()    // データ数だけ要素を生成
-    .append("tr")
-    .append("td")
-    .text(String)   // データ内容を出力
